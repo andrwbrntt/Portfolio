27b42fcdf3998eaec493743e6b59878a20319162
Andrew Barnett <144189873+andrwbrntt@users.noreply.github.com>
2024-01-19 12:41:01 -0600

    Create LEFTJOIN.sql

---
+++ LEFT JOIN/LEFTJOIN.sql
@@ -0,0 +1,31 @@
+/*
+Write a query that joins the two tables together
+displaying the customer's first name, last name
+and order total.
+
+Include customers whose order total is null.
+
+Order the output so that the customers whose
+order is null display first
+*/
+
+SELECT n.first_name, n.last_name, o.order_total
+FROM customer_names n
+	LEFT JOIN customer_orders o ON n.customer_id = o.customer_id
+ORDER BY o.order_total;
+
+/*
+Expected Output
+
+first_name      last_name        order_total
+Sarah	          Irving	
+Theresa	        Scott	
+Grant	          Barnett	          120
+Rob	            Gonzalez	        120
+Adam	          Johnson	          135
+Emily	          Jarvis	          150
+Brian	          Greyson	          240
+Jan	            Smith	            30
+Joseph	        James	            50
+Rachel	        Sweeney	          90
+*/
