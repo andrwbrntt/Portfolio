ca07dfa6c6a699875148a366199c29650ce68754
Andrew Barnett <144189873+andrwbrntt@users.noreply.github.com>
2024-01-30 18:52:02 -0600

    Update awards.sql

--- Sales Award Banquet/awards.sql
+++ Sales Award Banquet/awards.sql
@@ -1,6 +1,5 @@
-/*
-Now let's move on to finding which
-salespeople won which awards
+/* 
+Let's figure out which salespeople won which awards
 
 1. Rookie of the Year
  - 6 month cutoff
@@ -24,18 +23,21 @@ salespeople won which awards
 -- 1. Rookie of the Year --
 
 WITH rookie_of_the_year  AS (
-	SELECT
-		s.salesperson_id, s.start_date,
-		CONCAT(s.first_name, ' ', s.last_name) AS salesperson_name,
-       		 SUM(p.sold_price) AS revenue_earned,
-       		 LEAST(12, TIMESTAMPDIFF(MONTH, s.start_date, '2023-12-31')) AS tenure_in_months -- to help with proration calculation, this provides tenure by month, but limits to the first year --
-	FROM salespersons s
-   	JOIN sold_projects p ON s.salesperson_id = p.salesperson_id
-   	WHERE s.start_date BETWEEN '2022-06-01' AND '2023-05-31' -- limit to salespeople hired between these dates --
-		AND s.termination_date IS NULL -- limit to current employees
-       		AND p.sold_date BETWEEN s.start_date AND DATE_ADD(s.start_date, INTERVAL 1 YEAR) -- limit the revenue summed to first year sales --
-    	GROUP BY s.salesperson_id
-    	ORDER BY revenue_earned DESC
+  SELECT s.salesperson_id, s.start_date,
+  	CONCAT(s.first_name, ' ', s.last_name) AS salesperson_name,
+  	SUM(p.sold_price) AS revenue_earned,
+  	LEAST(12, TIMESTAMPDIFF(MONTH, s.start_date, '2023-12-31')) AS tenure_in_months 
+	-- Provides tenure in months, placing a limit of 1 year from the start date to help with proration calculation --
+  FROM salespersons s
+  JOIN sold_projects p ON s.salesperson_id = p.salesperson_id
+  WHERE s.start_date BETWEEN '2022-06-01' AND '2023-05-31' 
+	-- This will limit the salespeople to those hired within the cutoff parameters --
+	AND s.termination_date IS NULL 
+	-- This will limit the result to salespeople still with the company --
+       	AND p.sold_date BETWEEN s.start_date AND DATE_ADD(s.start_date, INTERVAL 1 YEAR) 
+	-- This will limit the revenue sum of each eligible salesperson to the parameter of their first year --
+  GROUP BY s.salesperson_id
+  ORDER BY revenue_earned DESC
 )
 
 SELECT *, ROUND(((revenue_earned / tenure_in_months) * 12)) AS proration
